02cbd113f187587f7e0a092e2301eb1f882e2415
Wes Chuang <icemilk0122@gmail.com>
2023-06-29 18:14:25 +0800

    Add interview

---
+++ src/components/Interview/Interview.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Video from "./Video";
+
+function Interview() {
+
+  return (
+    <>
+        interview
+      <Video />
+    </>
+  );
+}
+
+export default Interview;
